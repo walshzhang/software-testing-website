fabf71d8a9ed821fba56e095e5d1526bf0ff30c6
xiufeng.zhang <xiufeng.zhang@test.com>
2022-05-10 21:36:02 +0800

    docs: add navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,16 @@
+const {defaultTheme} = require('vuepress')
 module.exports = {
-    base: '/software-testing-website/'
+  base: '/software-testing-website/',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '讲义',
+        link: '/lecture'
+      }
+    ]
+  })
 }
